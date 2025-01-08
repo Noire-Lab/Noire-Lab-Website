006bb58427ebcb90e00f943e83ea6adbc2f6e4c1
Fresh-static-snow <praedythbr@gmail.com>
2025-01-08 18:49:52 +0200

    fix

--- src/app/components.client.tsx
+++ src/app/components.client.tsx
@@ -302,7 +302,7 @@ export const ProductItem: FC<ProductItemProps> = ({
           </figure>
         )}
         {type === "video" && (
-          <video controls loop muted preload="metadata">
+          <video controls loop muted preload="auto">
             <source className="w-full rounded-[8px]" src={link} />
           </video>
         )}
